1f0c9bb8dd6f586292cfee58d761443966469435
caique vitoriano <caiq-ferre@hotmail.com>
2018-06-06 21:43:15 -0300

    iniciando e passando arquivos

---
+++ src/main/java/controle/funcoes.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+import com.vividsolutions.jts.geom.Geometry;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author caiq-
+ */
+public final class funcoes {
+
+    public static boolean equals(JTextField geo1, JTextField geo2) {
+        return geo1.equals(geo2);    
+    }
+    
+  
+    
+}
